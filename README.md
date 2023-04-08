b4379081256990f9f3aad0e6edf5a769824fd35b
Gonzalo Perez <gon.fer10@gmail.com>
2023-04-08 14:45:44 -0300

    Fin Seccion 5: Nest CLI Resource - Brands CRUD

--- README.md
+++ README.md
@@ -22,6 +22,13 @@
   <!--[![Backers on Open Collective](https://opencollective.com/nest/backers/badge.svg)](https://opencollective.com/nest#backer)
   [![Sponsors on Open Collective](https://opencollective.com/nest/sponsors/badge.svg)](https://opencollective.com/nest#sponsor)-->
 
+# Car Dealship
+
+Populate DB
+```
+http://localhost:3000/seed
+```
+
 ## Description
 
 [Nest](https://github.com/nestjs/nest) framework TypeScript starter repository.
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -8,21 +8,11 @@ import { CreateCarDto, UpdateCarDto } from './dto/index';
 export class CarsService {
 
     private cars: Car[] = [
-        {
-            id: uuid(),
-            brand: 'Toyota',
-            model: 'Corolla'
-        },
-        {
-            id: uuid(),
-            brand: 'Honda',
-            model: 'Civic'
-        },
-        {
-            id: uuid(),
-            brand: 'Ford',
-            model: 'Focus'
-        },
+        //{
+        //    id: uuid(),
+        //    brand: 'Toyota',
+        //    model: 'Corolla'
+        //},
     ];
 
 
@@ -77,5 +67,9 @@ export class CarsService {
         const car = this.findOneById(id);
         
         this.cars = this.cars.filter( car => car.id !== id );
+    }
+
+    fillCarsWithSeedData ( cars: Car[] ){
+       this.cars = cars; 
     }
 }
